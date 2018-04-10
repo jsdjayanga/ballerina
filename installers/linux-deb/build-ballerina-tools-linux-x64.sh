1db8ec9faa9b04f4e30766d0e55d8fcd3887fe76
Jayanga Dissanayake <jsdjayanga@gmail.com>
2018-04-10 14:25:09 +0000

    Add deb files

---
+++ installers/linux-deb/build-ballerina-tools-linux-x64.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BALLERINA_VERSION=0.970.0-alpha1-SNAPSHOT
+BALZIP=/home/ubuntu/Packs/ballerina-tools-linux-$BALLERINA_VERSION.zip
+BALDIST=ballerina-tools-linux-$BALLERINA_VERSION
+
+echo $BALDIST "build started at" $(date +"%Y-%m-%d %H:%M:%S")
+
+rm -rf target
+mkdir -p target/original
+unzip $BALZIP -d target/original #> /dev/null 2>&1
+mv target/original/$BALDIST target/original/ballerina-linux-$BALLERINA_VERSION
+
+mkdir -p target/$BALDIST/opt/ballerina
+chmod -R 755 target/$BALDIST/opt/ballerina
+
+mv target/original/ballerina-linux-$BALLERINA_VERSION target/$BALDIST/opt/ballerina 
+
+cp -R linux/DEBIAN target/$BALDIST/DEBIAN
+sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALDIST/DEBIAN/postinst
+sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALDIST/DEBIAN/postrm
+sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALDIST/DEBIAN/control
+
+dpkg-deb --build target/$BALDIST
+
+echo $BALDIST "build completed at" $(date +"%Y-%m-%d %H:%M:%S")
+
+
+
